f18e4b7fb2947dcb0fbe24624a643a075c231da5
Anurag-NURA <anuragrawatz3k@gmail.com>
2024-08-15 22:26:12 +0530

    Videos.jsx map null error fixed

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -4,6 +4,8 @@ import { Stack, Box } from '@mui/material';
 import { VideoCard, ChannelCard } from './';
 
 export const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return 'Loading...';
+
   return (
     <Stack direction={direction || "row"} flexWrap="wrap" paddingY={2} justifyContent="center" gap={2} sx={{ px: 'auto' }}>
       {videos.map((item, index) => (
